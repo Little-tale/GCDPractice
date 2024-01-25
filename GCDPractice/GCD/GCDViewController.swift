d17380cc07c0eb3b8239fcc90bc31dfa0b661a38
judge_happily <you3192001@gmail.com>
2024-01-25 21:11:20 +0900

    연습장

--- GCDPractice/GCD/GCDViewController.swift
+++ GCDPractice/GCD/GCDViewController.swift
@@ -5,6 +5,8 @@ class GCDViewController: UIViewController {
     let buttonView = UIView()
     let syncButton = UIButton()
     let asyncButton = UIButton()
+    let serialAsyncButton = UIButton()
+    
     let imageUIView = UIView()
     
    
@@ -22,16 +24,65 @@ class GCDViewController: UIViewController {
         
         asyncButton.addTarget(self, action: #selector(showButtonAct), for: .touchUpInside)
         
+        serialAsyncButton.addTarget(self , action: #selector(showSerialAsyncAct), for: .touchUpInside)
+        
+        print("11111")
+        print("22222")
+        print("33333")
+        print("44444")
+        
+        // serialSync()
+        // serialAsync()
+        //concurrentSync()
+        //concurrentAsync()
     }
 
 }
-
+extension GCDViewController {
+    func serialSync() {
+        print("내가 바로 시리얼(직렬-> 한개의 쓰레드에 몰빵 ) 싱크 동기 -> 기다릴께")
+        DispatchQueue.main.sync {
+            print("항상 널 기다리고 있을께")
+        }
+        print("난 끝이야")
+    }
+    
+    func serialAsync() {
+        print("나도 시리얼 (직렬 -> 한개의 쓰레드에 몰빵) 어싱크 기다리지 않아")
+        for i in 1...50 {
+            DispatchQueue.main.async {
+                print(i,"이만큼 일했어")
+            }
+        }
+        print("난 끝이야 -> ")
+    }
+    
+    func concurrentSync() {
+        print("내가 바로 콘커런트 -> 분산이야 싱크 -> 기다릴께")
+        for i in 1...50 {
+            DispatchQueue.global().sync {
+                print("난 "+"\(i)"+" 일했어 하지만 언제끝날지 너가 알까? ")
+            }
+        }
+        print("난 끝이야 -> ")
+    }
+    
+    func concurrentAsync() {
+        print("내가 바로 콘커런트 -> 분산이야 싱크 -> 기다릴께")
+        for i in 1...50 {
+            DispatchQueue.global().async {
+                print("난 "+"\(i)"+" 일했어 하지만 언제끝날지 너가 알까? ")
+            }
+        }
+        print("난 끝이야 -> ")
+    }
+}
 
 
 
 extension GCDViewController {
     @objc func syncButtonAct(){
-        print("sync::1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣")
+        print("🎬sync::1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣🎬")
         for imageView in imageViewList {
             print("sync::2️⃣2️⃣2️⃣2️⃣2️⃣")
             showSyncImages(imageView: imageView)
@@ -41,47 +92,64 @@ extension GCDViewController {
     }
     
     func showSyncImages(imageView: UIImageView){
+        print("싱크시작 👺👺👺👺👺")
         do{
-            print("싱크시작 👺👺👺👺👺")
+            
             let data = try Data(contentsOf: NasaAssistant.getURL!)
             imageView.image = UIImage(data: data)
-            print("싱크끝 😸😸😸😸😸")
+           
         } catch {
             print("ERROR🔥🔥🔥🔥🔥🔥")
         }
-        
+        print("싱크끝 😸😸😸😸😸")
     }
     
     @objc func showButtonAct() {
         
-        print("🎬매인스택 요청 Async::1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣🎬")
+        print("🎬매인 요청 Async::1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣🎬")
         for imageView in imageViewList {
-            print("매인스택 요청 Async::2️⃣2️⃣2️⃣2️⃣2️⃣")
+            print("매인 반복문 Async::2️⃣2️⃣2️⃣2️⃣2️⃣")
             showAsyncImages(imageView: imageView)
-            print("매인스택 요청 Async::3️⃣3️⃣3️⃣3️⃣3️⃣")
+            print("매인 반복문끝 Async::3️⃣3️⃣3️⃣3️⃣3️⃣")
         }
-        print("✂️매인스택 요청 Async::4️⃣4️⃣4️⃣4️⃣4️⃣44✂️")
+        print("✂️매인 요청끝 Async::4️⃣4️⃣4️⃣4️⃣4️⃣44✂️")
     }
+    
     func showAsyncImages(imageView: UIImageView) {
+        print("어싱크시작 👺👺👺👺👺")
         DispatchQueue.global().async {
             do{
-                print("어싱크시작 👺👺👺👺👺")
+                print("111111111111111")
                 guard let url = NasaAssistant.getURL else { return }
                 let data = try Data(contentsOf: url)
-                
+                print("22222222222222")
                 DispatchQueue.main.async {
+                    print("Main 으로 다시....")
                     imageView.image = UIImage(data: data)
+                    print("이미지 보냈다,...!")
                 }
-                
-                print("어싱크끝 😸😸😸😸😸")
+               print("33333333333333")
             } catch {
                 print("ERROR🔥🔥🔥🔥🔥🔥")
-                
             }
             
         }
+        print("어싱크끝 😸😸😸😸😸")
         
     }
+    
+    
+    @objc func showSerialAsyncAct() {
+        print("🎬매인 요청 SerialAsync::1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣🎬")
+        for imageView in imageViewList {
+            print("매인 반복문 SerialAsync::2️⃣2️⃣2️⃣2️⃣2️⃣")
+            showAsyncImages(imageView: imageView)
+            print("매인 반복문끝 SerialAsync::3️⃣3️⃣3️⃣3️⃣3️⃣")
+        }
+        print("✂️매인 요청끝 SerialAsync::4️⃣4️⃣4️⃣4️⃣4️⃣44✂️")
+    }
+    
+    
     
 }
 
@@ -98,6 +166,7 @@ extension GCDViewController {
         
         buttonView.addSubview(syncButton)
         buttonView.addSubview(asyncButton)
+        buttonView.addSubview(serialAsyncButton)
         
         imageUIView.addSubview(firstImageView)
         imageUIView.addSubview(secondImageView)
@@ -105,9 +174,20 @@ extension GCDViewController {
         
         buttonView.backgroundColor = .systemGray2
         buttonView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 150)
-        syncButton.frame = CGRect(x: 20, y: buttonView.frame.height - 50, width: UIScreen.main.bounds.width / 2 - 40, height: 40)
-        asyncButton.frame = CGRect(x: buttonView.frame.width / 2 + 20, y: buttonView.frame.height - 50, width: UIScreen.main.bounds.width / 2 - 40, height: 40)
         
+        syncButton.frame = CGRect(x: 20, y: buttonView.frame.height - 50, width: UIScreen.main.bounds.width / 3 - 40, height: 40)
+        
+        asyncButton.frame = CGRect(x: buttonView.frame.width / 3 + 20, y: buttonView.frame.height - 50, width: UIScreen.main.bounds.width / 3 - 40, height: 40)
+        let width = asyncButton.frame.width
+        serialAsyncButton.frame = CGRect(x: buttonView.frame.width / 3 + width + 60 , y: buttonView.frame.height - 50, width: UIScreen.main.bounds.width / 3 - 40, height: 40)
+        
+        var config = UIButton.Configuration.filled()
+        
+        config.baseBackgroundColor = .brown
+        config.baseForegroundColor = .white
+        config.title = "sirial Async"
+        
+        serialAsyncButton.configuration = config
         
         
         var syncButtonConfigu = UIButton.Configuration.filled()
